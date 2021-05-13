c92b84c225af32db4c84a9f590b87a8efc5779d3
Prasadthegreat <prasadthegreat799@gmail.com>
2021-05-13 09:19:59 +0530

    "Added create classroom"

---
+++ app/src/main/java/com/prasadthegreat/infosnity/faculty/createclassActivity.java
@@ -0,0 +1,16 @@
+package com.prasadthegreat.infosnity.faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prasadthegreat.infosnity.R;
+
+public class createclassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createclass);
+    }
+}
