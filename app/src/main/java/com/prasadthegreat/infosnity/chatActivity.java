1b602c8e5dc352a14e68aa39f4a3e265ce5b849d
Prasadthegreat <prasadthegreat799@gmail.com>
2021-05-15 09:45:49 +0530

    "Added Emoji Loader In Chat Box"

--- app/src/main/java/com/prasadthegreat/infosnity/chatActivity.java
+++ app/src/main/java/com/prasadthegreat/infosnity/chatActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.vanniktech.emoji.EmojiPopup;
 
 public class chatActivity extends AppCompatActivity {
 
-
-
-
+    ImageView mEmoji;
     TextView mName;
+    EditText mMsgBox;
+    LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +26,21 @@ public class chatActivity extends AppCompatActivity {
 
         String name=getIntent().getExtras().getString("name");
         mName=(TextView)findViewById(R.id.name);
-
+        mEmoji=(ImageView)findViewById(R.id.emojiImg);
+        mMsgBox=(EditText)findViewById(R.id.msgBox);
+        linearLayout=(LinearLayout)findViewById(R.id.LinearLayout);
         mName.setText(name);
+
+
+        EmojiPopup popup=EmojiPopup.Builder.fromRootView(findViewById(R.id.Root_Layout)).build(mMsgBox);
+
+        mEmoji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                popup.toggle();
+            }
+        });
 
     }
 }
